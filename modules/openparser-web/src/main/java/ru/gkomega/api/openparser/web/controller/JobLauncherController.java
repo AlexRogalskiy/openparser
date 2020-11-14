d0aa1756d992c417f2dbcf95b6af40ae40789144
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2020-11-14 12:55:58 +0300

    Updates on xml/csv/file configuration

--- modules/openparser-web/src/main/java/ru/gkomega/api/openparser/web/controller/JobLauncherController.java
+++ modules/openparser-web/src/main/java/ru/gkomega/api/openparser/web/controller/JobLauncherController.java
@@ -3,11 +3,11 @@ package ru.gkomega.api.openparser.web.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -20,12 +20,13 @@ public class JobLauncherController {
     private final Job job;
 
     @GetMapping("/launchjob")
-    public String handle() {
+    public String handle(@RequestParam(value = "enable", required = false) final boolean enableParams) {
         try {
-            final JobParameters jobParameters = new JobParametersBuilder()
-                .addLong("time", System.currentTimeMillis())
-                .toJobParameters();
-            this.jobLauncher.run(this.job, jobParameters);
+            final JobParametersBuilder parametersBuilder = new JobParametersBuilder();
+            if (enableParams) {
+                parametersBuilder.addLong("time", System.currentTimeMillis());
+            }
+            this.jobLauncher.run(this.job, parametersBuilder.toJobParameters());
         } catch (Exception e) {
             log.info(e.getMessage());
         }
